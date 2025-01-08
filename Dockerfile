f02f7eb11d063a2407fa50a5b52d3743ebec5b02
Ubuntu <ubuntu@ip-172-31-24-81.ec2.internal>
2025-01-08 08:51:40 +0000

    docker file added

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Python runtime as the base image
+FROM python:3.9-slim
+
+# Set the working directory
+WORKDIR /app
+
+# Copy application code
+COPY . .
+
+# Install dependencies
+RUN pip install -r requirements.txt
+
+# Expose the application port
+EXPOSE 5000
+
+# Command to run the application
+CMD ["python", "application.py"]
+
---
+++ docker.sh
@@ -0,0 +1,22 @@
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt install docker.io -y
+docker --version         
+sudo usermod -aG docker $USER
+sudo chown $USER /var/run/docker.sock   # OR give the below permissions to docker.sock 
+#sudo chmod 777 /var/run/docker.sock
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
